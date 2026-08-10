36c1368c89f43bfe7dfc234d87178381998d6772
prateek <sandeep223022@gmail.com>
2026-08-11 03:53:58 +0530

    Implement PostHog

--- app/components/ExploreBtn.tsx
+++ app/components/ExploreBtn.tsx
@@ -1,11 +1,22 @@
 "use client";
 
 import Image from 'next/image';
+import posthog from "posthog-js";
 import React from 'react'
 
 const ExploreBtn = () => {
   return (
-   <button type='buttton' id='explore-btn' className='mt-7 mx-auto' onClick={() => console.log("clicked")}>
+   <button
+    type='button'
+    id='explore-btn'
+    className='mt-7 mx-auto'
+    onClick={() => {
+      if (process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN && process.env.NEXT_PUBLIC_POSTHOG_HOST) {
+        posthog.capture("events_explored");
+      }
+      console.log("clicked");
+    }}
+   >
     <a href="#events">
       Explore Events
       <Image src="/icons/arrow-down.svg" alt='arrow-down' 
