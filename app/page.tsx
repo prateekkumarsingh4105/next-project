16e48b12e75b46c51e9e1360110df33590752433
prateek <sandeep223022@gmail.com>
2026-08-19 17:57:47 +0530

    comply with v18

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,9 @@ import events from '@/lib/constants'
 const page = async () => {
   "use cache";
   cacheLife('hours')
-  // const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/events`)
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/events`)
 
-  // const { events }  = await response.json()
+  const { events }  = await response.json()
 
   return (
     <section>
@@ -21,7 +21,7 @@ const page = async () => {
     <div className="mtt-20 space-y-7">
       <h3>Featured Events</h3>
       <ul className="events">
-        { events && events.length > 0 &&  events.map((event) => (
+        { events && events.length > 0 &&  events.map((event: IEvent) => (
           <li key={event.title} className='list-none'>
             <EventCard  {...event}/>
           </li>
